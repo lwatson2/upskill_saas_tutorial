d548e03ad95e88b162c5ef9603190400dc483fe9
Logan Watson <watson.logan@yahoo.com>
2017-07-07 14:40:53 +0000

    Fixed spelling error

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default to: 'watson.logan@yahoo.com'
   
-  def contcact_email(name, email, body)
+  def contact_email(name, email, body)
     @name= name 
     @email= email
     @body= body
